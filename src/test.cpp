0b6f257eb074bf6cec0a4c9948e65e852e78309a
Advik <advik.b@gmail.com>
2024-10-22 20:44:43 +0530

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -5,6 +5,14 @@
 #include <lexbor/html/interfaces/document.h>
 #include <lexbor/html/interfaces/element.h>
 
+const char* string_c_str(lxb_char_t *) {
+    char *str = static_cast<char *>(malloc(sizeof(str)));
+    for (int i = 0; i < sizeof(str); i++) {
+        str[i] = (char) str[i];
+    }
+    return str;
+}
+
 const char* test() {
     static const lxb_char_t html[] = "<div>Works fine!</div>";
     size_t html_len = sizeof(html) - 1;
@@ -22,6 +30,7 @@ const char* test() {
     const lxb_char_t *tag_name = lxb_dom_element_qualified_name(lxb_dom_interface_element(document->body),
                                                                 NULL);
 
+
     lxb_html_document_destroy(document);
-    return std::string(reinterpret_cast<const char *>(tag_name)).c_str();
+    return string_c_str(const_cast<lxb_char_t *>(tag_name));
 }
