283c6df0191bfd36f77bd2022270835dca64cceb
Advik <advik.b@gmail.com>
2024-10-22 23:40:34 +0530

    Create cpr_backend.hpp

---
+++ include/z-library/backend/cpr_backend.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Advik on 22-10-2024.
+//
+
+#ifndef CPR_BACKEND_HPP
+#define CPR_BACKEND_HPP
+#ifdef USE_CPR
+
+#include <string>
+#include <z-library/backend/z-backend-scaffold.hpp>
+
+namespace z_library {
+    // CPR Backend which inherits from z_cache
+    class cpr_backend : z_backend_scaffold {
+        std::string get_data(const std::string &url);
+    };
+}
+
+#endif //CPR_BACKEND_HPP
+#endif //USE_CPR
