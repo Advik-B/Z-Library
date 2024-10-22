1d4ec4e89dc6505a139798e981672910e0a3b671
Advik <advik.b@gmail.com>
2024-10-22 23:46:47 +0530

    Update z-backend-scaffold.hpp

--- include/z-library/backend/z-backend-scaffold.hpp
+++ include/z-library/backend/z-backend-scaffold.hpp
@@ -10,14 +10,13 @@
 namespace z_library {
     class z_backend_scaffold {
     private:
-        z_backend_scaffold() = default;
-        ~z_backend_scaffold() = default;
-
         // void set(const std::string &key, const std::string &value);
         // std::string get(const std::string &key);
         // void delete_cache(const std::string &key);
         // void clear_cache();
     public:
+        z_backend_scaffold() = default;
+        ~z_backend_scaffold() = default;
         std::string get_data(const std::string &url);
     };
 }
