8002823b1a9a2b82d72b26fd99b0ec5234a450a4
Advik <advik.b@gmail.com>
2024-10-22 20:14:57 +0530

    Test the html parser library, lexabor

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,26 @@
 #include "test.hpp"
+#include <lexbor/html/parser.h>
+#include <lexbor/html/interfaces/document.h>
 
 const char* test() {
+    static const lxb_char_t html[] = "<div>Works fine!</div>";
+    size_t html_len = sizeof(html) - 1;
+
+    lxb_html_document_t *document = lxb_html_document_create();
+    if (document == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    lxb_status_t status = lxb_html_document_parse(document, html, html_len);
+    if (status != LXB_STATUS_OK) {
+        exit(EXIT_FAILURE);
+    }
+
+    const lxb_char_t *tag_name = lxb_dom_element_qualified_name(lxb_dom_interface_element(document->body),
+                                                                NULL);
+
+    printf("Element tag name: %p\n", tag_name);
+
+    lxb_html_document_destroy(document);
     return "Hello, World!";
 }
